b2fd6bd200b61d0d1ce6c7ae56f01a5efd88bef0
yotamzf <yotam.zeevi.federman@gmail.com>
2024-09-10 04:38:18 +0300

    Updating Recipe entity, setting empty string for firebase.

--- app/src/main/java/com/example/goodfoodapp/models/Recipe.kt
+++ app/src/main/java/com/example/goodfoodapp/models/Recipe.kt
@@ -6,9 +6,9 @@ import androidx.room.PrimaryKey
 @Entity(tableName = "recipes")
 data class Recipe(
     @PrimaryKey val recipeId: String,
-    val title: String,
-    val picture: String,  // Recipe picture URL
-    val content: String,
-    val uploadDate: Long, // Timestamp in milliseconds
-    val userId: String    // ID of the user who posted the recipe
+    val title: String="",
+    val picture: String="",  // Recipe picture URL
+    val content: String="",
+    val uploadDate: Long=0L, // Timestamp in milliseconds
+    val userId: String=""    // ID of the user who posted the recipe
 )
